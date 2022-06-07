21b56019bb12203a53f0d79314494ef2769eb413
Mike Spade <mikespadeny@gmail.com>
2022-06-06 21:57:49 -0600

    Create foundations.js

---
+++ js/foundations.js
@@ -0,0 +1,10 @@
+// first lesson in javascript. how to use let and const to assign variables. using the log method from the console. using script tags to access js withing your html
+// literals vs variables vs expressions
+
+let x = 100 + 50
+
+let x = a + b
+
+let x = 10(a + b)
+
+
